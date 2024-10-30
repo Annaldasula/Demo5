7e2e5f0adad66886a012295e9c7d413367e37160
Annaldasula <102715951+Annaldasula@users.noreply.github.com>
2024-10-30 10:33:34 +0530

    Update Demo5app.py

--- Demo5app.py
+++ Demo5app.py
@@ -904,7 +904,7 @@ News search: All Articles: entity mentioned at least once in the article"""
 
     if st.sidebar.button("Download PowerPoint"):
         # List of DataFrames to save
-        dfs = [Entity_SOV1, sov_dt, pubs_table, Jour_table, PType_Entity, PP_table, ppe1]
+        dfs = [Entity_SOV3, sov_dt1, pubs_table, Jour_table, PType_Entity, PP_table]
         table_titles = ['SOV Table', 'Month-on-Month Table', 'Publication Table', 'Journalist Table',
                     'Pub Type and Entity Table', 'Pub Type and Pub Name Table',
                     'PubType PubName and Entity Table']
